e24b25c6a091400575961720ea7a6631735ff915
zanna241 <57253764+zanna241@users.noreply.github.com>
2026-08-27 06:58:39 +0200

    V0.9.9.3 bench mode: update include/version.h

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
 #pragma once
-#define FW_VERSION "0.9.9.2"
-#define FW_TAG "V0992"
-#define FW_BUILD_NAME "BMW 520xd MONITOR V0.9.9.2 STABILITY"
+#define FW_VERSION "0.9.9.3"
+#define FW_TAG "V0993"
+#define FW_BUILD_NAME "BMW 520xd MONITOR V0.9.9.3 BENCH"
